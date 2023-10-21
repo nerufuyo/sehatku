a6ef196bb5d35bbba368b6f2bb7ee385924bc407
nerufuyo <listyoadipamungkas1401@gmail.com>
2023-10-21 16:06:27 +0700

    ✨feature: addtionaly components

---
+++ lib/presentation/style/pallet.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF002060);
+const Color secondary = Color(0xFF00D9D5);
+
+const Color accent10 = Color(0xFFFF7200);
+const Color accent20 = Color(0xFFEB0004);
+
+const Color white0 = Color(0xFFFFFFFF);
+const Color white10 = Color(0xFFBEBEBE);
+const Color white20 = Color(0xFF6A6A6A);
+
+const Color green10 = Color(0xFFB3FFCB);
+const Color green20 = Color(0xFF007025);
---
+++ lib/presentation/style/typography.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+const TextStyle heading1 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle heading2 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle heading3 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle heading4 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle heading5 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle heading6 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle subHeading1 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subHeading2 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subHeading3 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subHeading4 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subHeading5 = TextStyle(
+  fontFamily: 'Gilroy',
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle body1 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle body2 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle body3 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle body4 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle body5 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 10,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle body6 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 8,
+  fontWeight: FontWeight.bold,
+);
+
+const TextStyle subBody1 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subBody2 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subBody3 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subBody4 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subBody5 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle subBody6 = TextStyle(
+  fontFamily: 'Proxima Nova',
+  fontSize: 8,
+  fontWeight: FontWeight.w500,
+);
